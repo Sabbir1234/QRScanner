02985697cb5e1d05ae230dab053e6a500a816412
Sabbir Hossain <sabbir.hossain@upaybd.com>
2023-11-22 09:45:25 +0600

    - Initiated

---
+++ QRScanner/QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by SABBIR HOSSAIN on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
